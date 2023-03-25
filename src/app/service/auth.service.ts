3b84bee5f871e5b54b1df0346697339eb396a127
lithdoh <lithifuse@gmail.com>
2023-03-24 20:25:53 -0700

    Add heroku url

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -9,7 +9,9 @@ export class AuthService {
   constructor(private http: HttpClient) {
 
   }
-  apiurl = 'http://localhost:3000/user';
+  // apiurl = 'http://localhost:3000/user';
+  apiurl = 'https://authentication-server-app.herokuapp.com/user';
+  
 
   RegisterUser(inputdata: any) {
     return this.http.post(this.apiurl, inputdata)
@@ -24,7 +26,9 @@ export class AuthService {
     return this.http.put(this.apiurl + '/' + id, inputdata);
   }
   getuserrole() {
-    return this.http.get('http://localhost:3000/role');
+    // return this.http.get('http://localhost:3000/role');
+    return this.http.get('https://authentication-server-app.herokuapp.com/role');
+    
   }
   isloggedin() {
     return sessionStorage.getItem('username') != null;
@@ -33,9 +37,13 @@ export class AuthService {
     return sessionStorage.getItem('role') != null ? sessionStorage.getItem('role')?.toString() : '';
   }
   GetAllCustomer() {
-    return this.http.get('http://localhost:3000/customer');
+    // return this.http.get('http://localhost:3000/customer');
+    return this.http.get('https://authentication-server-app.herokuapp.com/customer');
+    
   }
   Getaccessbyrole(role: any, menu: any) {
-    return this.http.get('http://localhost:3000/roleaccess?role=' + role + '&menu=' + menu)
+    // return this.http.get('http://localhost:3000/roleaccess?role=' + role 
+    return this.http.get('https://authentication-server-app.herokuapp.com/roleaccess?role=' + role 
+    + '&menu=' + menu)
   }
 }
